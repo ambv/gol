b112dbe4326fb6434dae57d8abdb6e5f16f7905f
Łukasz Langa <lukasz@langa.pl>
2023-07-13 00:06:43 +0200

    Show per-worker progress, require minimum triangle area, save every frame

--- reduce_climb.py
+++ reduce_climb.py
@@ -2,13 +2,16 @@ import asyncio
 import concurrent.futures
 from contextvars import ContextVar
 from dataclasses import dataclass
+import multiprocessing
+from pathlib import Path
 import os
 import random
+from typing import cast
 
 import click
 import numpy as np
 from PIL import Image, ImageDraw
-from rich.progress import Progress
+from rich.progress import Progress, TaskID
 from rich.console import Console
 
 
@@ -22,6 +25,18 @@ class ColorTriangle:
     color: RGB
     triangle: Triangle
     difference: int
+
+
+@dataclass
+class ProgressUpdate:
+    completed: int
+    total: int
+
+    def done(self):
+        return self.completed == self.total
+
+
+ProgressDict = dict[TaskID, ProgressUpdate]
 
 
 console = Console()
@@ -39,7 +54,20 @@ def calculate_difference(image1: Image.Image, image2: Image.Image) -> int:
 
 
 def generate_candidate_triangle(
-    max_x: int, max_y: int, mutate: Triangle | None = None
+    max_x: int, max_y: int, mutate: Triangle | None = None, min_area: float = 0.0
+) -> Triangle:
+    area = -1.0
+    triangle = None
+    while area < min_area or triangle is None:
+        triangle = _generate_candidate_triangle(max_x, max_y, mutate)
+        area = calculate_triangle_area(triangle)
+    return triangle
+
+
+def _generate_candidate_triangle(
+    max_x: int,
+    max_y: int,
+    mutate: Triangle | None = None,
 ) -> Triangle:
     if mutate is not None:
         x = [p[0] for p in mutate]
@@ -93,6 +121,9 @@ def choose_next_triangle(
     current_difference: int,
     last_triangle: ColorTriangle | None,
     attempts: int,
+    min_area: float,
+    progress_dict: ProgressDict,
+    task_id: TaskID,
 ) -> ColorTriangle:
     input_image = INPUT_IMG.get()
     output_image = OUTPUT_IMG.get()
@@ -111,11 +142,12 @@ def choose_next_triangle(
     best_triangle = None
     best_color = None
 
+    update = progress_dict[task_id]
     iterations = 0
-    while smallest_difference > 0 and iterations < attempts:
+    while iterations < attempts:
         iterations += 1
         candidate_triangle = generate_candidate_triangle(
-            width, height, mutate=best_triangle
+            width, height, mutate=best_triangle, min_area=min_area
         )
         candidate_color = get_average_color(input_image, candidate_triangle)
         new_output_image = apply_triangle(
@@ -126,6 +158,8 @@ def choose_next_triangle(
             smallest_difference = new_difference
             best_triangle = candidate_triangle
             best_color = candidate_color
+        update.completed = iterations
+        progress_dict[task_id] = update  # shared dict needs explicit assignment
 
     if best_triangle is not None and best_color is not None:
         return ColorTriangle(
@@ -135,11 +169,36 @@ def choose_next_triangle(
     raise LookupError(f"Couldn't improve the score in {attempts} iterations")
 
 
+def save_image_with_triangles(
+    image: Image.Image,
+    path: Path | str,
+    *,
+    diff_with: Image.Image,
+    triangles: list[ColorTriangle],
+) -> None:
+    for i, t in enumerate(triangles):
+        image = apply_triangle(image, t.triangle, t.color)
+        actual = calculate_difference(diff_with, image)
+        if actual != t.difference:
+            print(
+                f"warning: invalid difference at triangle {i}."
+                f" Expected: {t.difference}, actual: {actual}"
+            )
+    image.save(path)
+
+
+def path_with_index(path: str | Path, index: int) -> Path:
+    p = Path(path)
+    s = p.suffix
+    n = p.with_suffix("").name
+    return p.with_name(n + f"_{index:03}").with_suffix(s)
+
+
 def init_images(input_path: str) -> None:
     input_image = Image.open(input_path)
     width, height = input_image.size
     average_color: RGB = input_image.resize((1, 1), resample=Image.LANCZOS).getpixel((0, 0))  # type: ignore
-    output_image = Image.new("RGB", (width, height), average_color)
+    output_image = Image.new("RGB", (width, height), average_color)  # type: ignore
     INPUT_IMG.set(input_image)
     OUTPUT_IMG.set(output_image)
 
@@ -149,6 +208,8 @@ async def reduce_image_to_output(
     output_path: str,
     num_triangles: int = 500,
     attempts_per_triangle: int = 1000,
+    max_retries: int = 3,
+    min_area_pct: float = 0.01,
 ) -> None:
     init_images(input_path)
     triangles: list[ColorTriangle] = []
@@ -156,20 +217,35 @@ async def reduce_image_to_output(
 
     input_image = INPUT_IMG.get()
     output_image = OUTPUT_IMG.get()
+    width, height = input_image.size
+    image_area = width * height
     current_difference = calculate_difference(input_image, output_image)
 
     with (
+        multiprocessing.Manager() as manager,
         concurrent.futures.ProcessPoolExecutor(
             initializer=init_images,
             initargs=(input_path,),
             max_workers=CPU_COUNT,
         ) as pool,
-        Progress(console=console, auto_refresh=False) as progress,
+        Progress(console=console) as progress,
     ):
-        task_id = progress.add_task("Generating...", total=num_triangles)
-        while len(triangles) < num_triangles:
-            print(f"{len(triangles)} triangles so far...")
-            last_triangle = triangles[-1] if triangles else None
+        _progress = cast(ProgressDict, manager.dict())
+        _tasks: list[TaskID] = []
+        for i in range(CPU_COUNT):
+            _tasks.append(progress.add_task(f"task {i+1}", visible=False))
+            _progress[_tasks[i]] = ProgressUpdate(
+                completed=0, total=attempts_per_triangle
+            )
+
+        retries = 0
+        overall = progress.add_task(
+            "Generating...", total=num_triangles * CPU_COUNT * attempts_per_triangle
+        )
+        while len(triangles) < num_triangles and retries < max_retries:
+            last_triangle = triangles[-1] if triangles and retries == 0 else None
+            percentage = len(triangles) / num_triangles
+            min_area = min_area_pct * (image_area - percentage * image_area)
             attempts = [
                 loop.run_in_executor(
                     pool,
@@ -177,33 +253,77 @@ async def reduce_image_to_output(
                     current_difference,
                     last_triangle,
                     attempts_per_triangle,
+                    min_area,
+                    _progress,
+                    _tasks[i],
                 )
-                for _ in range(CPU_COUNT)
+                for i in range(CPU_COUNT)
             ]
 
+            async def monitor() -> None:
+                base = len(triangles) * CPU_COUNT * attempts_per_triangle
+                while True:
+                    await asyncio.sleep(0.1)
+                    current = 0
+                    for task_id, update_data in _progress.items():
+                        # update the progress bar for this task:
+                        progress.update(
+                            task_id,
+                            completed=update_data.completed,
+                            total=update_data.total,
+                            visible=not update_data.done(),
+                        )
+                        current += update_data.completed
+                    progress.update(
+                        overall,
+                        description=f"{len(triangles)} triangles so far...",
+                        completed=base + current,
+                    )
+
+            empty = 0
             triangle: ColorTriangle | None = None
-            for attempt in asyncio.as_completed(attempts):
-                try:
-                    candidate = await attempt
-                    if triangle is None or triangle.difference > candidate.difference:
-                        triangle = candidate
-                except LookupError:
-                    print("  One worker didn't find anything")
-                    continue
+            monitor_task = asyncio.create_task(monitor(), name="monitor")
+            try:
+                for attempt in asyncio.as_completed(attempts):
+                    try:
+                        candidate = await attempt
+                        if (
+                            triangle is None
+                            or triangle.difference > candidate.difference
+                        ):
+                            triangle = candidate
+                    except LookupError:
+                        empty += 1
+                        continue
+            finally:
+                monitor_task.cancel()
             if triangle is None:
                 print("  No candidates in this round")
-                break
+                retries += 1
+                continue
 
+            retries = 0
             triangles.append(triangle)
+            delta_difference = current_difference - triangle.difference
             current_difference = triangle.difference
-            print(f"  Difference improved to {current_difference}")
-            progress.update(task_id, advance=1)
+
+            maybe_empty = ""
+            if empty:
+                maybe_empty = f" with {empty} empty results"
+            print(
+                f"  Difference improved to {current_difference}"
+                f" (delta {delta_difference}) {maybe_empty}"
+            )
+            p = path_with_index(output_path, len(triangles))
+            save_image_with_triangles(
+                output_image, p, diff_with=input_image, triangles=triangles
+            )
 
     # Save the final output image
-    for t in triangles:
-        apply_triangle(output_image, t.triangle, t.color)
-    output_image.save(output_path)
     print(f"Output image saved to {output_path}")
+    save_image_with_triangles(
+        output_image, output_path, diff_with=input_image, triangles=triangles
+    )
 
 
 @click.command()
