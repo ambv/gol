80a538488155d0a9e1a1cccc6ed62b9c9485e9f7
Łukasz Langa <lukasz@langa.pl>
2023-07-16 22:26:12 +0200

    Pre-defined grid instead of np.random.choice

--- gol_matplotlib.py
+++ gol_matplotlib.py
@@ -19,11 +19,63 @@ class GameOfLife:
 
     def __post_init__(self):
         self.fig, self.ax = plt.subplots()
-        self.grid = np.random.choice(
-            [0, 1],
-            size=(self.rows, self.columns),
-            p=[0.5, 0.5],
-        )
+        self.grid = np.zeros((self.rows, self.columns))
+
+        g = self.grid
+
+        # pulsar
+        g[2, 4:7] = 1
+        g[2, 10:13] = 1
+        g[4:7, 2] = 1
+        g[4:7, 7] = 1
+        g[4:7, 9] = 1
+        g[4:7, 14] = 1
+        g[7, 4:7] = 1
+        g[7, 10:13] = 1
+        g[9, 4:7] = 1
+        g[9, 10:13] = 1
+        g[10:13, 2] = 1
+        g[10:13, 7] = 1
+        g[10:13, 9] = 1
+        g[10:13, 14] = 1
+        g[14, 4:7] = 1
+        g[14, 10:13] = 1
+
+        # pentadecathlon
+        g[5, 24:27] = 1
+        g[6, 24] = 1
+        g[6, 26] = 1
+        g[7:11, 24:27] = 1
+        g[11, 24] = 1
+        g[11, 26] = 1
+        g[12, 24:27] = 1
+
+        # LWSS
+        g[20, 3:5] = 1
+        g[21, 2:6] = 1
+        g[22, 2:4] = 1
+        g[22, 5:7] = 1
+        g[23, 4:6] = 1
+
+        # MWSS
+        g[30, 13:16] = 1
+        g[31, 12:17] = 1
+        g[32, 12:15] = 1
+        g[32, 16:18] = 1
+        g[33, 15:17] = 1
+
+        # HWSS
+        g[40, 23:27] = 1
+        g[41, 22:28] = 1
+        g[42, 22:26] = 1
+        g[42, 27:29] = 1
+        g[43, 26:28] = 1
+
+        # glider
+        g[40, 6:8] = 1
+        g[41, 7:9] = 1
+        g[42, 6] = 1
+
         self.image = self.ax.imshow(
             self.grid,
             interpolation="nearest",
@@ -62,12 +114,13 @@ def main():
     gol = GameOfLife(rows=50, columns=50)
     anim = animation.FuncAnimation(
         gol.fig,
-        gol.update,
+        gol.update, # lambda x: [gol.image], # gol.update,
         frames=900,
         interval=100,
         blit=True,
     )
-    anim.save("gol.mp4", fps=30, dpi=150, extra_args=["-vcodec", "libx264"])
+    anim.save("gol-figures.mp4", fps=10, dpi=150, extra_args=["-vcodec", "libx264"])
+    # plt.show()
 
 
 if __name__ == "__main__":
